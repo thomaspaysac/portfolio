f2e38ebaafd90fb47744fdeecdd857cc782dce60
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2024-02-27 20:30:23 +0100

    Delete animated icon component

--- app/ui/header/Header.tsx
+++ app/ui/header/Header.tsx
@@ -6,7 +6,6 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { montserrat } from "@/app/lib/fonts";
-import HeaderAnimatedIcon from "./icon";
 
 const links = [
   { name: 'À propos', href: '/'},
