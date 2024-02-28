5b9522f87370c6981ac5518124e2ca9d71db2905
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2024-02-28 21:50:46 +0100

    Make header responsive

--- app/ui/header/Header.tsx
+++ app/ui/header/Header.tsx
@@ -17,27 +17,32 @@ export default function Header () {
   const pathname = usePathname();
 
   return (
-    <header className="flex gap-6 items-center py-4 px-20 bg-zinc-800 text-gray-50">
-      <Image src={'/header_icon.png'} width={48} height={48} unoptimized={true} alt='' className="mr-auto" />
-      {
-        links.map((link, i) => {
-          return (
-            <Link 
-              key={i} 
-              href={link.href}
-              className={clsx(
-                'nav-link',
-                {
-                "active underline underline-offset-8 decoration-4 font-bold" : pathname === link.href || pathname.includes("portfolio") && link.href === '/portfolio'
-              })}
-            >
-                {link.name}
-            </Link>
-          )
-        })
-      }
-      <Link href="/contacts">
-        <button className="bg-indigo-500 px-5 py-2 font-bold text-white rounded transition-all hover:bg-indigo-600 hover:cursor-pointer">
+    <header className="flex flex-col md:flex-row gap-4 md:gap-6 items-center py-4 px-10 md:px-20 bg-zinc-800 text-gray-50">
+      <div className="flex w-full gap-4 justify-center md:w-fit items-center mr-auto">
+        <Image src={'/header_icon.png'} width={48} height={48} unoptimized={true} alt='' />
+        <h1 className="font-bold md:hidden">Thomas Paysac</h1>
+      </div>
+      <div className="flex w-full md:w-fit justify-between gap-6">
+        {
+          links.map((link, i) => {
+            return (
+              <Link
+                key={i}
+                href={link.href}
+                className={clsx(
+                  'nav-link w-max',
+                  {
+                  "active underline underline-offset-8 decoration-4 font-bold" : pathname === link.href || pathname.includes("portfolio") && link.href === '/portfolio'
+                  })}
+              >
+                  {link.name}
+              </Link>
+            )
+          })
+        }
+      </div>
+      <Link href="/contacts" className="w-full md:w-fit">
+        <button className="bg-indigo-500 w-full px-5 py-2 font-bold text-white rounded transition-all hover:bg-indigo-600 hover:cursor-pointer">
           Contactez-moi
         </button>
       </Link>
