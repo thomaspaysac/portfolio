4a5f62cb1e68f2b3826a0d31ed5a0cefddf58d73
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2024-03-09 22:16:25 +0100

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,45 +11,50 @@ export const metadata: Metadata = {
 export default async function Page () {
   const articles = await getAllArticles();
 
-  return (
-    <div className="flex min-h-[calc(100svh-80px)] flex-col p-4 py-10 md:px-24 md:py-12 gap-2 text-white">
-      <h1 className="text-4xl tracking-wide font-semibold">Blog</h1>
-      <div>Cette page est vide {'('}pour le moment{')'}.</div>
-
-            <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-              {articles && articles.map((article: any) => (
-                  <article key={article.sys.id} className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden">
-                    <Image
-                      alt="placeholder"
-                      className="aspect-[4/3] object-cover w-full"
-                      height="263"
-                      src={article.articleImage? article.articleImage.url : ''}
-                      width="350"
-                    />
-                    <div className="flex-1 p-6">
-                      <Link href={`/blog/${article.slug}`}>
-                        <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
-                          {article.title}
-                        </h3>
-                      </Link>
-                      <div className="inline-block rounded-full bg-zinc-100 px-3 py-1 text-sm font-semibold text-zinc-800">
-                        {article.category}
-                      </div>
-                      <p className="max-w-none text-zinc-500 mt-4 mb-2 text-sm dark:text-zinc-400">
-                        {article.summary}
-                      </p>
-                      <div className="flex justify-end">
-                        <Link
-                          className="inline-flex h-10 items-center justify-center text-sm font-medium"
-                          href={`/blog/${article.slug}`}
-                        >
-                          Read More →
+  if (!articles) {
+    return null;
+  } else {
+    return (
+      <div className="flex min-h-[calc(100svh-80px)] flex-col p-4 py-10 md:px-24 md:py-12 gap-2 text-white">
+        <h1 className="text-4xl tracking-wide font-semibold">Blog</h1>
+        <div>Cette page est vide {'('}pour le moment{')'}.</div>
+  
+              <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+                {articles.map((article: any) => (
+                    <article key={article.sys.id} className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden">
+                      <Image
+                        alt="placeholder"
+                        className="aspect-[4/3] object-cover w-full"
+                        height="263"
+                        src={article.articleImage? article.articleImage.url : ''}
+                        width="350"
+                      />
+                      <div className="flex-1 p-6">
+                        <Link href={`/blog/${article.slug}`}>
+                          <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
+                            {article.title}
+                          </h3>
                         </Link>
+                        <div className="inline-block rounded-full bg-zinc-100 px-3 py-1 text-sm font-semibold text-zinc-800">
+                          {article.category}
+                        </div>
+                        <p className="max-w-none text-zinc-500 mt-4 mb-2 text-sm dark:text-zinc-400">
+                          {article.summary}
+                        </p>
+                        <div className="flex justify-end">
+                          <Link
+                            className="inline-flex h-10 items-center justify-center text-sm font-medium"
+                            href={`/blog/${article.slug}`}
+                          >
+                            Read More →
+                          </Link>
+                        </div>
                       </div>
-                    </div>
-                  </article>
-              ))}
-            </div>
-    </div>
-  )
+                    </article>
+                ))}
+              </div>
+      </div>
+    )
+  
+  }
 };
