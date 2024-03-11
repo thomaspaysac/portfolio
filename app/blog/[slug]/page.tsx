60be70de552695ad325a5d5b201c93b0d35a9538
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2024-03-11 21:24:30 +0100

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -23,7 +23,7 @@ export async function generateMetadata(
       ],
     },
     twitter: {
-      card: 'summary_large_image',
+      card: 'summary',
       title: `${article.title} | Thomas Paysac`,
       description: article.summary,
       siteId: '1767197636539392000',
