1c8b9dd6af39452b9911fc91471229b0c31e1f48
thomaspaysac <113386819+thomaspaysac@users.noreply.github.com>
2024-03-14 07:22:57 +0100

    Style article content

--- app/ui/contacts/form.tsx
+++ app/ui/contacts/form.tsx
@@ -35,7 +35,7 @@ export default function Form () {
         <textarea id="message" name="message" rows={10} minLength={5} maxLength={1000} required className="w-full bg-zinc-300 text-black p-2 mt-2 rounded-lg"></textarea>
       </div>
       <div className={error ? "block bg-red-500 p-2 mb-2 rounded-lg w-fit transition-all" : "hidden"}>Une erreur est survenue, veuillez réessayer.</div>
-      <button className="bg-indigo-500 p-2 rounded-lg transition-colors hover:bg-indigo-600">Envoyer</button>
+      <button className="bg-indigo-500 px-4 py-2 font-semibold rounded-lg transition-colors hover:bg-indigo-600">Envoyer</button>
       <div className={messageSent ? 'inline w-fit p-3 ml-4 rounded-lg bg-emerald-500 transition-all' : 'hidden'}>Votre message a bien été envoyé.</div>
     </form>
   )
